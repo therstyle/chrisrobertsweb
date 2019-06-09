9f9c937c0deb06e3a0bbff89bccb5c807f5a21e6
Chris Roberts <chris@829llc.com>
2019-06-09 15:27:18 -0400

    Added timeline elements

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -3,8 +3,8 @@
     <Heading title="Resume"></Heading>
 
     <div class="resume--content">
-      <div class="timline">
-        
+      <div class="timeline">
+        <Timeline-Entry></Timeline-Entry>
       </div>
 
       <aside>
@@ -16,11 +16,13 @@
 
 <script>
 import Heading from './layout/Heading.vue';
+import TimelineEntry from './layout/TimelineEntry.vue';
 
 export default {
   name: 'Resume',
   components: {
-    Heading
+    Heading,
+    TimelineEntry
   }
 }
 </script>
@@ -34,5 +36,67 @@ export default {
   .resume--content {
     padding: 0 4vw;
     display: flex;
+  }
+
+  .timeline {
+    position: relative;
+    margin: 0 7vw;
+
+    &:before {
+      content: '';
+      position: absolute;
+      left: 0;
+      width: 1px;
+      height: 100%;
+      background: var(--light-gray);
+    }
+  }
+
+  .timeline--entry {
+    padding: 4.1vw;
+    border-bottom: 1px solid var(--light-gray);
+
+    header {
+      display: flex;
+      align-items: center;
+      margin-bottom: 1.3vw;
+
+      img {
+          margin-right: 1.3vw;
+      }
+
+      .company-info {
+        line-height: 90%;
+
+        > :first-child {
+        margin-bottom: 0;
+        line-height: 1;
+      }
+      }
+    }
+  }
+
+  .details {
+    padding-left: 0;
+    margin-bottom: 0;
+
+    > li {
+      font-size: 16px;
+      line-height: 1.5;
+      list-style: none;
+      position: relative;
+      display: flex;
+      align-items: flex-start;
+
+      &:before {
+        content: '';
+        display: block;
+        width: 12px;
+        height: 12px;
+        border-radius: 100%;
+        background: var(--red);
+        margin-right: 10px;
+      }
+    }
   }
 </style>
---
+++ src/components/layout/TimelineEntry.vue
@@ -0,0 +1,31 @@
+<template>
+  <article class="timeline--entry">
+    <header>
+      <img src="https://dummyimage.com/42/fff/fff" alt="">
+      <div class="company-info">
+        <h3>829 Studios</h3>
+        <small>Senior Front End Developer</small>
+      </div>
+    </header>
+
+    <ul class="details">
+      <li>Develop and maintain custom WordPress themes for clients utilizing PHP, JavaScript, SASS/CSS</li>
+      <li>Use REST APIs to develop SPAs with Vue.JS and React</li>
+      <li>Work closely with design team to maintain design and web
+development standards</li>
+      <li>Mentor, assist, and provide code reviews for junior web developers</li>
+    </ul>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Timeline-Entry'
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
+
