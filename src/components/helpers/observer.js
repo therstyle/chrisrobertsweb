ab9d87e202f12a2e47a277ae0b5f50fd1b731706
Chris Roberts <chris@829llc.com>
2019-07-07 16:35:43 -0400

    Added threshold to observer function

--- src/components/helpers/observer.js
+++ src/components/helpers/observer.js
@@ -2,7 +2,10 @@ export default {
   data() {
     return {
       observer: null,
-      parentName: ''
+      parentName: '',
+      config: {
+        threshold: 0.25
+      }
     }
   },
   methods: {
@@ -16,12 +19,12 @@ export default {
           if(entry.isIntersecting) {
             if (this.$vnode.elm.id) {
               this.parentName = this.$vnode.elm.id;
-              console.log(this.parentName + ' MIXIN TEST');
+              console.log(this.parentName);
               this.intersected(this.parentName);
             }
           }
         });
-      });
+      }, this.config);
 
       this.observer.observe(this.$el);
     }
