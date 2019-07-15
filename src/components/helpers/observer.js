d740a0b0178a95cd64044ec267c5d5861a6be74e
Chris Roberts <chris@829llc.com>
2019-07-15 16:54:01 -0400

    Added function to detect mobile viewport

--- src/components/helpers/observer.js
+++ src/components/helpers/observer.js
@@ -2,6 +2,7 @@ export default {
   data() {
     return {
       observer: null,
+      isMobile: '(max-width: 992px)',
       sectionName: '',
       config: {
         threshold: 0.25
@@ -26,6 +27,15 @@ export default {
       }, this.config);
 
       this.observer.observe(this.$el);
+    },
+    detectMobile() {
+      if (window.matchMedia(this.isMobile).matches) {
+        this.config.threshold = 0;
+        console.log('mobile');
+      }
     }
+  },
+  mounted() {
+    //this.detectMobile();
   }
 }
