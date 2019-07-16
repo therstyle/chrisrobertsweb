def733e4ad2613a809b3ca266b21d9a2c52b9d0d
Chris Roberts <chris@829llc.com>
2019-07-16 08:27:24 -0400

    Added php mail handler

---
+++ public/mail.php
@@ -0,0 +1,18 @@
+<?php
+//decode string received from fetch
+$content = trim(file_get_contents("php://input"));
+$data = json_decode($content, true);
+if (isset($_POST['to']) && isset($_POST['name']) && isset($_POST['message'])) {
+  $to = $_POST['to'];
+  $name = $_POST['name'];
+  $message = $_POST['message'];
+  $from = 'noreply@rstyledesign.com';
+  $subject = 'Test Contact Email';
+  $headers = 'From: '.$name.' < '.$from.' >\n';
+  mail($to, $subject, $message, $headers);
+  echo '<span class="success">A test email has been sent to '.$to.'!</span>';
+}
+else {
+  echo '<span class="error">A problem has occurred. Please try again later.</span>';
+}
+?>
