791f2cb8b89d5dbf0218dc1bd965a96945c08af0
Chris Roberts <chris@829llc.com>
2019-07-23 13:40:48 -0400

    Updated mailer function

--- public/mail.php
+++ public/mail.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 //decode string received from fetch
 $content = trim(file_get_contents("php://input"));
 $data = json_decode($content, true);
