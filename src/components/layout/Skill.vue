ebaa809528a8a4239d346c6fcdf08d573e3a0a08
Chris Roberts <chris@829llc.com>
2019-08-08 06:24:25 -0400

    Added start year to JSON file + experience function

--- src/components/layout/Skill.vue
+++ src/components/layout/Skill.vue
@@ -4,6 +4,7 @@
       <img :src="logo" :alt="name">
       <h6>{{ name }}</h6>
       <small>{{ years }} Years</small>
+      {{ experience() }}
     </div>
   </li>
 </template>
@@ -11,6 +12,14 @@
 <script>
 export default {
   name: 'Skill',
+  methods: {
+    experience(startYear) {
+      const date = new Date();
+      const currentYear = date.getFullYear();
+
+      console.log(currentYear);
+    }
+  },
   props: {
     logo: String,
     name: String,
