f1aa69ee826b6dcefbec29d556e04a835cb86372
therstyle <therstyle@gmail.com>
2019-08-17 11:23:14 -0400

    Registered stats component

---
+++ src/components/layout/Stats.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="stats">
+  
+  </footer>  
+</template>
+
+<script>
+export default {
+  name: "Stats",
+  props: {
+    stats: Array
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
