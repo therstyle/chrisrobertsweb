82c146012a36493ae53c62934b55cf876be3b4cb
Chris Roberts <chris@829llc.com>
2019-08-25 14:34:35 -0400

    Installed universal analytics + cleanup

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-//import VueAnalytics from 'vue-analytics'
-
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
-
-// Vue.use(VueAnalytics, {
-//   id: 'UA-9665180-9'
-// })
